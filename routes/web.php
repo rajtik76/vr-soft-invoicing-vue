a1fae7e17a1e704323a4afe5953b9427026a8477
Vladislav Rajtmajer <rajtik@gmail.com>
2023-05-12 22:15:07 +0200

    - add barryvdh/laravel-debugbar - remove Welcome.vue + Dashboard.vue pages and routes - add TaskController.php + route + Task.vue page - change default page in RouteServiceProvider.php - set default route to the login page - switch to psr12 preset for Laravel Pint

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,22 +17,19 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return to_route('login');
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
+// Auth
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// Auth + verified
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::resource('/task', TaskController::class);
 });
 
 require __DIR__.'/auth.php';
