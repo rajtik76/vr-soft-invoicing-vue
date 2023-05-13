093ae638556de7d2464e3e39f22b91b1acdbe4d0
Vladislav Rajtmajer <rajtik@gmail.com>
2023-05-13 12:16:03 +0200

    #1 - filter only active user task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\TaskResource;
 use App\Models\Task;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -12,7 +13,11 @@ class TaskController extends Controller
     public function index(): Response
     {
         return Inertia::render('Task', [
-            'paginator' => TaskResource::collection(Task::withSum('spentTimes', 'time')->paginate()),
+            'paginator' => TaskResource::collection(
+                Task::withSum('spentTimes', 'time')
+                    ->where('user_id', Auth::id())
+                    ->paginate()
+            ),
         ]);
     }
 }
